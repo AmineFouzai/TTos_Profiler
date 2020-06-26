70779c3ab7cfd66edc510e28586a3a79c0ab802d
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-06-26 21:43:02 +0100

    Update NORMALIZE.py

--- NORMALIZE.py
+++ NORMALIZE.py
@@ -5,7 +5,7 @@ import  os
 import cv2
 import numpy as np
 
-DATA_SET='data_set'
+DATA_SET='data_set/'
 for directory in os.listdir(DATA_SET):
     for image in os.listdir(f'{DATA_SET}/{directory}'):
         if image.endswith('.jpg'):
